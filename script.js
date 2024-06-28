4d2b51c9f28434ae2162b3cd4ceb7b2b50bac386
AMAN KUMAR SHARMA <144410366+Aman-Sharma404@users.noreply.github.com>
2024-06-28 20:14:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let fullImgBox = document.getElementById("fullImgBox");
+console.log(fullImgBox);
+let fullImg = document.getElementById("fullImg");
+
+function openfullImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+
+}
+function closefullImg() {
+    fullImgBox.style.display = "none";
+}
